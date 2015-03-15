dfe0eaa3fa5d269a54cca1d326620f927f773f25
gyaeh <mange.selin@gmail.com>
2015-03-15 14:18:45 +0100

    added template

---
+++ meta/template.cpp
@@ -0,0 +1,46 @@
+// File:    template.cpp
+// Author:  Magnus Selin
+// Updated: 2015-03-15
+
+#include <iostream> 
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <string>
+#include <bitset>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+#define rep(i, a, b) for(int i = (a); i < int(b); ++i)
+#define trav(it, v)  for(typeof((v).begin()) it = (v).begin(); it != (v).end(); ++it)
+#define output_vi(a) rep(ii, 0, a.size()) printf("%d\t", (a)[ii]); printf("\n");
+#define pb(a) push_back(a);
+
+typedef double fl;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector< vector<int> > vvi;
+typedef vector<int>::iterator vii;
+
+
+bool solve(){
+
+	return true;
+}
+
+int main(){
+	int tc=1; //scanf("%d", &tc);
+	rep(i, 0, tc) solve();
+    vi a;
+
+	return 0;
+}
